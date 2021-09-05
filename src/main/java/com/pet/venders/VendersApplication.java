09f054f4b5003539f8cbcf0fc6fd4522643bb861
wmarvyn <wmarvyn@gmail.com>
2021-09-05 11:14:10 -0300

    Criando o projeto

---
+++ src/main/java/com/pet/venders/VendersApplication.java
@@ -0,0 +1,13 @@
+package com.pet.venders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/venders/VendersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.venders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
