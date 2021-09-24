4426c8bac593cd202a5b0247c7a3818116d26933
wmarvyn <wmarvyn@gmail.com>
2021-09-24 08:48:43 -0300

    Criando a classe de dominio Categoria

---
+++ src/main/java/com/pet/venders/resources/CategoriaResources.java
@@ -0,0 +1,26 @@
+package com.pet.venders.resources;
+
+import com.pet.venders.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="categorias")
+public class CategoriaResources {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2,"Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+    }
+}
